3ef16788b98889719be4894c729c51e6d46ecd86
Mishra <rmishr46@LAMU02XHQG7JG5J.uhc.com>
2020-03-12 15:10:41 +0530

    Python string use cases demo

---
+++ eclipse-workspace_my_work/PythonBasics/src/name.py
@@ -0,0 +1,27 @@
+name = "python crash course"
+print(name.title())
+print(name.capitalize())
+print(name.upper())
+print(name.split(" "))
+print(name.center(50))
+print(name.count("c"))
+print(name.endswith("se"))
+print(name.endswith("er"))
+print(name.index("c"))
+print(name.replace("crash", "not crash"))
+
+"""
+Output 
+
+Python Crash Course
+Python crash course
+PYTHON CRASH COURSE
+['python', 'crash', 'course']
+               python crash course                
+2
+True
+False
+7
+python not crash course
+
+"""
